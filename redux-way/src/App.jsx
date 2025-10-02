b52b6b888582e30bfb197539e511c330d4505425
MosfiqurRubel <rubel.airin@gmail.com>
2025-10-02 11:57:26 +0600

    Creating - store

--- redux-way/src/App.jsx
+++ redux-way/src/App.jsx
@@ -1,12 +1,14 @@
+import { Provider } from 'react-redux';
 import Counter from "@/components/Counter";
+import store from '@/redux/store';
 
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Counter />
       <Counter />
       <Counter />
-    </>
+    </Provider>
   );
 }
 
