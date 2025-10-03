71bcbcd76d4a4a35b923086a98ddcb50ed5a6565
MosfiqurRubel <rubel.airin@gmail.com>
2025-10-04 05:57:12 +0600

    Own props with the connect API

--- redux-way/src/App.jsx
+++ redux-way/src/App.jsx
@@ -2,12 +2,15 @@ import { Provider } from "react-redux";
 import store from "@/redux/store";
 import HooksCounter from "@/components/HooksCounter";
 import DynamicHooksCounter from "@/components/DynamicHooksCounter";
+import VariableCounter from "@/components/VariableCounter";
 
 function App() {
   return (
     <Provider store={store}>
       <HooksCounter />
       <DynamicHooksCounter />
+      <VariableCounter />
+      <VariableCounter dynamic />
     </Provider>
   );
 }
