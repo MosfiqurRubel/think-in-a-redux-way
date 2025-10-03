19df8bd3b02d3666144301f118089e257ccc6c80
MosfiqurRubel <rubel.airin@gmail.com>
2025-10-04 04:43:26 +0600

    Fixed - git branch - not maintanable

--- redux-way/src/App.jsx
+++ redux-way/src/App.jsx
@@ -5,7 +5,7 @@ import HooksCounter from "@/components/HooksCounter";
 function App() {
   return (
     <Provider store={store}>
-      <HooksCounter />
+      <HooksCounter id={1} />
     </Provider>
   );
 }
