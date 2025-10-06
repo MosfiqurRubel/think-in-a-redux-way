ec92031d03b265d82ce911ce8c4fd370f425dc90
MosfiqurRubel <rubel.airin@gmail.com>
2025-10-07 03:18:37 +0600

    Defining reducers - todo Slice (Design reducer for todo feature)

--- todo_redux/src/redux/filters/actions.js
+++ todo_redux/src/redux/filters/actions.js
@@ -13,8 +13,6 @@ export const colorChanged = (color, changeType) => {
 export const statusChanged = (status) => {
   return {
     type: STATUS_CHANGED,
-    payload: {
-      status,
-    },
+    payload: status,
   };
 };
