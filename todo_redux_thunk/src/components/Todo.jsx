ccb78833015a6ddf2c83d83bcf085e6d636c15fa
MosfiqurRubel <rubel.airin@gmail.com>
2025-10-13 21:42:55 +0600

    Delete todo from server

--- todo_redux_thunk/src/components/Todo.jsx
+++ todo_redux_thunk/src/components/Todo.jsx
@@ -1,7 +1,7 @@
 import { useDispatch } from "react-redux";
-import { deleted } from "@/redux/todos/actions";
 import updateStatus from "@/redux/todos/thunk/updateStatus";
 import updateColor from "@/redux/todos/thunk/updateColor";
+import deleteTodo from "@/redux/todos/thunk/deleteTodo";
 import cancleImage from "@/assets/images/icons/cancel.png";
 import SvgIcon from "@/components/ui/SvgIcon";
 import Checkbox from "@/components/ui/Checkbox";
@@ -19,13 +19,13 @@ const Todo = ({ todo }) => {
   };
 
   const handleDelete = (todoId) => {
-    dispatch(deleted(todoId));
+    dispatch(deleteTodo(todoId));
   };
 
   return (
     <div className="flex justify-start items-center p-2 hover:bg-gray-100 hover:transition-all space-x-4 border-b border-gray-400/20 last:border-0">
       <div
-        className={`reletive rounded-full bg-white border-2 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${
+        className={`relative rounded-full bg-white border-2 w-5 h-5 flex flex-shrink-0 justify-center items-center mr-2 ${
           completed && "border-green-500 focus-within:border-green-500"
         }`}
       >
