4ff1fb2b8f55b29b3499e3aae34092f030f4ccfb
MosfiqurRubel <rubel.airin@gmail.com>
2025-10-18 00:35:04 +0600

    Add new feature - Dynamic Counter
    Add dynamic counter feature/slice

--- rtk/index.js
+++ rtk/index.js
@@ -1,5 +1,8 @@
 const store = require("./app/store");
 const { counterActions } = require("./features/counter/counterSlice");
+const {
+  dynamicCounterActions,
+} = require("./features/dynamicCounter/dynamicCounterSlice");
 
 // initial state
 console.log(`Initial State: ${JSON.stringify(store.getState())}`);
@@ -9,6 +12,11 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch(counterActions.increment());
-store.dispatch(counterActions.increment());
-store.dispatch(counterActions.decrement());
+// dispatch actions
+// store.dispatch(counterActions.increment());
+// store.dispatch(counterActions.increment());
+// store.dispatch(counterActions.decrement());
+
+store.dispatch(dynamicCounterActions.increment(3));
+store.dispatch(dynamicCounterActions.increment(4));
+store.dispatch(dynamicCounterActions.decrement(2));
