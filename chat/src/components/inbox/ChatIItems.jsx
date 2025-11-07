ee75a67d140cf00c18fc193b84c9aea06e1a9d40
MosfiqurRubel <rubel.airin@gmail.com>
2025-11-07 21:52:59 +0600

    Conversation Functionality
    Implement Conversations feature in UI

--- chat/src/components/inbox/ChatIItems.jsx
+++ chat/src/components/inbox/ChatIItems.jsx
@@ -1,30 +1,68 @@
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useGetConversationsQuery } from "@/features/conversations/conversationsAPI";
+import moment from "moment";
+import gravatarUrl from "gravatar-url";
 import ChatItem from "./ChatItem";
+import BookLoader from "@/components/ui/loaders/BookLoader";
+import Error from "@/components/ui/Error";
+import Loading from "@/components/ui/Loading";
+import getPartnerInfo from "@/utils/getPartnerInfo";
 
 const ChatIItems = () => {
-  return (
-    <ul>
+  const { user } = useSelector((state) => state.auth);
+  const { email } = user || {};
+  const {
+    data: conversations,
+    isLoading,
+    isError,
+    error,
+  } = useGetConversationsQuery(email);
+
+  // decide what to render
+  let content = null;
+  if (isLoading) {
+    content = (
       <li>
-        <ChatItem
-          avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-          name="Saad Hasan"
-          lastMessage="bye"
-          lastTime="25 minutes"
-        />
-        <ChatItem
-          avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-          name="Sumit Saha"
-          lastMessage="will talk to you later"
-          lastTime="10 minutes"
-        />
-        <ChatItem
-          avatar="https://cdn.pixabay.com/photo/2018/09/12/12/14/man-3672010__340.jpg"
-          name="Mehedi Hasan"
-          lastMessage="thanks for your support"
-          lastTime="15 minutes"
-        />
+        <BookLoader />
       </li>
-    </ul>
-  );
+    );
+  } else if (!isLoading && isError) {
+    content = (
+      <li>
+        <Error children={error?.data} />
+      </li>
+    );
+  } else if (!isLoading && !isError && conversations?.length === 0) {
+    content = (
+      <li>
+        <Loading loadingText="No conversations found!" />
+      </li>
+    );
+  } else if (!isLoading && !isError && conversations?.length > 0) {
+    content = conversations.map((conversation) => {
+      const { id, message, timestamp } = conversation;
+      const { name, email: partnerEmail } = getPartnerInfo(
+        conversation.users,
+        email
+      );
+
+      return (
+        <li key={id}>
+          <Link to={`/inbox/${id}`}>
+            <ChatItem
+              avatar={gravatarUrl(partnerEmail, { size: 80 })}
+              name={name}
+              lastMessage={message}
+              lastTime={moment(timestamp).fromNow()}
+            />
+          </Link>
+        </li>
+      );
+    });
+  }
+
+  return <ul>{content}</ul>;
 };
 
 export default ChatIItems;
