d7f68d93f8f38b8833cd66d9d6a5f84c3ad016c2
MosfiqurRubel <rubel.airin@gmail.com>
2025-11-07 09:38:59 +0600

    Passing Authorization Header to every request

--- chat/src/features/api/apiSlice.js
+++ chat/src/features/api/apiSlice.js
@@ -5,6 +5,14 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({
     // baseUrl: process.env.REACT_APP_API_URL,
     baseUrl: import.meta.env.VITE_API_URL,
+    prepareHeaders: async (headers, { getState, endpoint }) => {
+      console.log("getState ", getState());
+      console.log("endpoint", endpoint);
+      const token = getState()?.auth?.accessToken;
+      if (token) headers.set("Authorization", `Bearer ${token}`);
+
+      return headers;
+    },
   }),
   tagTypes: [],
   endpoints: (builder) => ({}),
