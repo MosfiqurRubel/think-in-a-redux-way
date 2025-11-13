7cdfffe29b5a2a8f658e5995dd746a0b92a1a233
MosfiqurRubel <rubel.airin@gmail.com>
2025-11-13 11:20:42 +0600

    Send Message - inside Conversation - Add conversation and message
    Add conversation and message

--- chat/src/components/inbox/chatbody/ChatBody.jsx
+++ chat/src/components/inbox/chatbody/ChatBody.jsx
@@ -24,7 +24,7 @@ const ChatBody = () => {
       <>
         <ChatHead message={messages[0]} />
         <Messages messages={messages} />
-        <Options />
+        <Options info={messages[0]} />
       </>
     );
   }
